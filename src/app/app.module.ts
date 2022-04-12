c18a105d308c07a207894d1f847b1cc6252a64b5
Develper-Gitonga <aizensalim52@gmail.com>
2022-04-12 15:43:21 +0300

    Deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { QuoteComponent } from './quote/quote.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteLikeComponent } from './quote-like/quote-like.component';
+import { FormsModule } from '@angular/forms';
 import { QuoteLikesComponent } from './quote-likes/quote-likes.component';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component'; 
 import { DateCountPipe } from './date-count.pipe';
 import { HighlightDirective } from './highlight.directive';
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
     QuoteComponent,
-    QuoteLikeComponent,
     QuoteLikesComponent,
     QuoteFormComponent,
     DateCountPipe,
@@ -21,7 +19,8 @@ import { HighlightDirective } from './highlight.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
